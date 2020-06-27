b598f6ce5e6ecbed07fc5b0b22cad8f5809479d6
hjavelar <hjavelar@gmail.com>
2020-06-26 22:18:07 -0300

    Corrigida orientação do ProgressBar da vazão de saída
    - Corrigida orientação do ProgressBar da vazão de saída
    - Desabilitado o controle quando o sistema é ligado
        -- o Slider MV não varia a PV quando o sistema está desligado
    - inserido alguns comentários nos scripts

--- Controle_Nivel/Scenes/Planta/Controlador_base.gd
+++ Controle_Nivel/Scenes/Planta/Controlador_base.gd
@@ -1,28 +1,42 @@
 extends Node
 
-export var enable = true
-export (NodePath) var _planta
+# A propriedade "enable", permite desabilitar a saida (MV=0), quando "false"
+export var enable = false
 
+# A propriedade "_planta" é o caminho para planta na qual será controlada
+# Ver: Planta_base
+export (NodePath) var _planta
+# Ao inicializar converte o caminho da planta em objeto
 onready var planta = get_node(_planta)
 
+# Variavéis comuns a controladores 
 var MV = 0.0
 var SP = 0.0
+# A variável de processo (PV)) é obtida automaticamente da "planta"
 
+# Sinal para atualizar elementos externos sobre a alteração da MV
 signal MV_changed(MV)
 
+# ### Verificar se é necessário: ###
 func _ready():
 	pass # Replace with function body.
 
+# A cada ciclo (de duração "delta"):
 func _physics_process(delta):
+	# Se o objeto "planta" está pronto (se foi atribuido, se existe)
 	if planta:
+		# Se a opção "enable" é true
 		if enable:
+			# Faz o cálculo da nova MV, tendo como entrada a SP, PV e delta
 			CalcMV(SP, planta.PV,delta)
 		else:
+			# Se a opção "enable" é false, então MV = 0
 			MV = 0.0
+		# Emite o sinal para atulizar elementos externos
 		emit_signal("MV_changed",MV)
+		# Atualiza planta sobre a nova MV
 		planta.MV = MV
-		
-#virtual
+
+# virtual (Essa função será substituída por controles "filhos" desse script)
 func CalcMV(SP:float,PV:float, delta:float):
 	pass
-		
--- Controle_Nivel/Scenes/Planta/Planta_base.gd
+++ Controle_Nivel/Scenes/Planta/Planta_base.gd
@@ -1,26 +1,26 @@
 extends Node
 
-
+# Toda planta (ou sistema) terá uma entrada (MV) e uma saída (PV)
 var MV = 0.0 setget setMV
 var PV = 0.0 setget setPV
 
+# Os sinais são gerados quando ocorrer variação nos valores de MV e PV
 signal PV_changed(newPV)
 signal MV_changed(newMV)
 
-
-func _physics_process(delta):	
+# executa CalcPV() a cada intervalo delta
+func _physics_process(delta):
 	CalcPV(delta)
 
 func setMV(value):
 	MV = value 
 	emit_signal("MV_changed",MV)
 
-
 func setPV(value):
 	PV = value
 	emit_signal("PV_changed",PV)
 
-# virtual
+# virtual (a função CalcPV será definida na Planta_nivel, baseada em Planta_base)
 func CalcPV(delta):
 	pass
 
